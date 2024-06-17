7deee86f9f44d4ed0aa19321fb4fc9121e339457
ILakshitha <ilakshitha7921@gmail.com>
2024-06-17 19:33:58 +0530

    Create class as component

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+class Myframe extends JFrame{
+  Myframe(){
+      JLabel lebal = new JLabel();
+      lebal.setText("Ishan Hello World");
+      lebal.setHorizontalAlignment(JLabel.CENTER);
+      lebal.setVerticalAlignment(JLabel.TOP);
+      lebal.setForeground(Color.RED);
+
+      this.add(lebal);
+  }
+}
 
 class Main{
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+
+        Border border = BorderFactory.createLineBorder(Color.RED);
+
+        Myframe frame = new Myframe();
+
+        JLabel lebal = new JLabel();
+        //lebal.setText("Hello World");
+        lebal.setBorder(border);
 
         frame.setTitle("My First Java GUI");
         frame.setSize(420,420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+
+
+        frame.add(lebal);
+
+
     }
 }
