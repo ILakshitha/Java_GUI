6efa72054dc9d979ea4d099e3aa4fabd8d10cf2f
ILakshitha <ilakshitha7921@gmail.com>
2024-06-17 18:30:54 +0530

    inizialize java GUI

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+class Main{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.setTitle("My First Java GUI");
+        frame.setSize(420,420);
+        frame.setVisible(true);
+    }
+}
